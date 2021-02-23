f44e1cf16bff1c2bcefcfefaf5530edd18040807
a.trzewik@t-systems.com <a.trzewik@t-systems.com>
2021-02-23 15:06:25 +0100

    gh-pages

---
+++ precache-manifest.5e2f2f9d0b3bc742ae37061c5e004abd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d846a192975f1bc927cf",
+    "url": "/guitar-notes-fretboard/css/chunk-vendors.544857bd.css"
+  },
+  {
+    "revision": "eb8106c4a26cb1cfc9feb723aa8b22f3",
+    "url": "/guitar-notes-fretboard/index.html"
+  },
+  {
+    "revision": "3ecfb811a872e67d1da5",
+    "url": "/guitar-notes-fretboard/js/app.b602c1ea.js"
+  },
+  {
+    "revision": "d846a192975f1bc927cf",
+    "url": "/guitar-notes-fretboard/js/chunk-vendors.7f1b0892.js"
+  },
+  {
+    "revision": "5ba4aca0884843aa15d190264d8734d8",
+    "url": "/guitar-notes-fretboard/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/guitar-notes-fretboard/robots.txt"
+  }
+]);
